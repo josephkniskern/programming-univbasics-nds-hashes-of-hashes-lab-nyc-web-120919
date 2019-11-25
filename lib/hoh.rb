ba0be86bad6e00deae83c8eea3ebbe9e7327c98b
Joseph Kniskern <kniskernjoseph@yahoo.com>
2019-11-25 22:54:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,13 +31,31 @@
 # }
 
 def naming_system
-  
-  name = {
-    PORTION_1,
-    PORTION_2,
-    PORTION_3,
-    PORTION_4
-  }
+   PORTION_1 = {
+   label: "Kingdom",
+  sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class"
+     }
+   }
+ }
+ 
+ PORTION_2 = {
+   label: "Order"
+ }
+ 
+ PORTION_3 = {
+   label: "Family",
+   sub_category: {
+     label: "Genus",
+   }
+ }
+ 
+ PORTION_4 = {
+   label: "Species",
+   sub_category: nil
+ }
   
   # Remember:
   #  Kingdom
